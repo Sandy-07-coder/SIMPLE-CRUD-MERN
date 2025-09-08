1f6e91fbef903a2e2fd45d7265635b49c852509f
Santhosh <santhoshprogrammer07@gmail.com>
2025-09-08 10:19:28 +0530

    updated logo and ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,8 +26,9 @@ function App() {
 
   return (
     <main className=' h-screen flex flex-col justify-evenly items-center bg-gray-900'>
-      <div>
-        <h1 className=' text-xl text-white'></h1>
+      <div className=' flex flex-row-reverse gap-4'>
+        <h1 className=' text-2xl text-white font-bold'>MERN CRUD APP</h1>
+        <img className=' size-12 pb-4' src="/app-logo.png" alt="" />
       </div>
       <AddUser />
       <UserTable />
